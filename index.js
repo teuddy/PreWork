ed1c0593406b714c2365f29b205bc8b83608381c
teuddycr <88355800+teuddycr@users.noreply.github.com>
2023-08-30 21:58:50 -0400

    palindrome changes

---
+++ index.js
@@ -0,0 +1,12 @@
+// amd workign on palindrome word
+
+// tested and validated
+const reverseString = (str) => {
+  return str.split("").reverse().join("");
+};
+
+const getPalindrome = (str) => {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, "");
+  return reverseString(lowRegStr) === lowRegStr;
+};
